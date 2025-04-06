e94f46c956f4a3554ab2c65755027c95c4ae93df
ftotherank <102555981+ftotherank@users.noreply.github.com>
2025-04-06 20:55:32 +0300

    a tiny change

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import MyFooter from './homes/MyFooter';
 import Checkout from './pages/Checkout';
-import Shop from './shops/Shop'; // Import Shop component or other components as needed
+import Shop from './shops/Shop'; 
+
 
 function App() {
   return (
     <>
       <Navbar />
       <div className="min-h-screen">
-        <Routes>
-          <Route path="/" element={<Shop />} /> {/* Default route */}
-          <Route path="/checkout" element={<Checkout />} />
-          {/* Add other routes here as needed */}
-        </Routes>
+        <Outlet />
       </div>
       <MyFooter />
     </>
